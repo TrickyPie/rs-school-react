df274a21e594649041cdcc12ec543d0d7151a619
Trickypie <klimova.pr@gmail.com>
2023-03-12 20:10:29 +0300

    feat: change link to navlink and add styles for link

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Link, Outlet } from 'react-router-dom';
 
 export class Layout extends React.Component {
-  render() {
+  private setActive(): (props: { isActive: boolean }) => string {
+    return ({ isActive }): string => {
+      return isActive ? 'navigation-link--active' : 'navigation-link';
+    };
+  }
+
+  public render(): JSX.Element {
     return (
       <>
         <header className="header">
           <nav className="navigation">
             <ul className="navigation-list">
               <li className="navigation-item">
-                <Link to="/">Home</Link>
+                <NavLink to="/" className={this.setActive()}>
+                  Home
+                </NavLink>
               </li>
               <li className="navigation-item">
-                <Link to="/about-us">About us</Link>
+                <NavLink to="/about-us" className={this.setActive()}>
+                  About us
+                </NavLink>
               </li>
             </ul>
           </nav>
