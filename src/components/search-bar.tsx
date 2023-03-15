a94b81f5d06c6571dbe55f6a47f05c2cfa88a220
Trickypie <klimova.pr@gmail.com>
2023-03-15 18:34:58 +0300

    feat: add search class

---
+++ src/components/search-bar.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+type SearchState = {
+  searchValue: string;
+};
+
+export class Search extends React.Component {
+  public state: SearchState = {
+    searchValue: localStorage.getItem('searchValue') || '',
+  };
+
+  public componentWillUnmount(value?: string) {
+    localStorage.setItem('searchValue', value || this.state.searchValue);
+  }
+
+  private handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.componentWillUnmount(event.target.value);
+    this.setState({ searchValue: event.target.value });
+  };
+
+  public render() {
+    const { searchValue } = this.state;
+    return (
+      <div className="search-bar-wrapper">
+        <div className="search-bar">
+          <input
+            className="search-bar__input"
+            type="text"
+            value={searchValue}
+            onChange={this.handleChange}
+          />
+          <i className="search-bar__icon" />
+        </div>
+        <button className="search-bar__button">Search</button>
+      </div>
+    );
+  }
+}
