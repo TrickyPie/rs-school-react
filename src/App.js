247e0ac551ca9da2a851c0b0323f3d761995d715
Trickypie <klimova.pr@gmail.com>
2023-03-19 21:06:24 +0300

    feat: create build

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AppRouter from './router';
-class App extends React.Component {
-    render() {
-        return (React.createElement(React.Fragment, null,
-            React.createElement(AppRouter, null)));
-    }
-}
-export default App;
--- src/pages/notfound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-class NotFound extends React.Component {
-    render() {
-        return (React.createElement("div", null,
-            React.createElement("h2", { className: "page-not-found" }, "Page Not Found")));
-    }
-}
-export default NotFound;
