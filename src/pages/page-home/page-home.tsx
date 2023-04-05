8799231741a80f2ab38c6170dc2a47b7ea2aeaa2
Trickypie <klimova.pr@gmail.com>
2023-04-05 21:54:23 +0300

    feat: update main page and add api data in it

--- src/pages/page-home/page-home.tsx
+++ src/pages/page-home/page-home.tsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './page-home-style.css';
 import Search from '../../components/search/Search';
 import { Cards } from '../../components/cards/Cards';
-/* import cardData from '../../mock/mock';
- */
+import { Plant } from '../../components/card/Card';
+
 const MainPage: React.FC = () => {
+  const [plants, setPlants] = useState<Plant[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect((): void => {
+    fetch('https://my-json-server.typicode.com/TrickyPie/react-api/items')
+      .then((response) => response.json())
+      .then((data) => {
+        setPlants(data);
+        console.log(data, plants);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  }, [plants]);
+
+  const handleSearchTermChange = (value: string): void => {
+    setSearchTerm(value);
+    console.log(value);
+  };
+
   return (
     <>
-      <Search />
+      <Search onSearchTermChange={handleSearchTermChange} />
       <div className="cards-container">
-        <Cards /* cardData={cardData} */ />
+        <Cards plants={plants} searchTerm={searchTerm} />
       </div>
     </>
   );
