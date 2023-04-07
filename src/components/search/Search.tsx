8f798be0123840f12d09b9cac029f17012a65c18
Trickypie <klimova.pr@gmail.com>
2023-04-07 23:36:53 +0300

    feat: update search component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,24 +2,25 @@ import React, { useState, useEffect, KeyboardEvent } from 'react';
 import './search-style.css';
 import search from '../../assets/png/search.png';
 
-type SearchProps = {
-  onSearchTermChange: (value: string) => void;
+type Props = {
+  onSearchTermChange: (searchTerm: string) => void;
 };
 
-const Search: React.FC<SearchProps> = (props) => {
+const Search: React.FC<Props> = ({ onSearchTermChange }) => {
   const [searchValue, setSearchValue] = useState<string>(localStorage.getItem('searchValue') || '');
 
   useEffect(() => {
     const value = localStorage.getItem('searchValue');
     if (value !== null) {
       setSearchValue(value);
+      onSearchTermChange(value);
     }
-  }, []);
+  }, [onSearchTermChange]);
 
   const handleEnter = (event: KeyboardEvent<HTMLInputElement>): void => {
     if (event.key === 'Enter') {
       localStorage.setItem('searchValue', searchValue);
-      props.onSearchTermChange(searchValue);
+      onSearchTermChange(searchValue);
     }
   };
 
