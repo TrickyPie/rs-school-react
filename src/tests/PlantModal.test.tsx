7165bb288b5771ca053b60e3db1202445c3a5125
Trickypie <klimova.pr@gmail.com>
2023-04-10 02:52:43 +0300

    feat: add tests for modal

--- src/tests/PlantModal.test.tsx
+++ src/tests/PlantModal.test.tsx
@@ -19,11 +19,51 @@ describe('PlantModal', () => {
     const title = await findByTestId(document.body, 'plant-title');
     const description = await findByTestId(document.body, 'plant-description');
     const closeButton = await findByTestId(document.body, 'close-button');
-
-    await waitFor(() => {
+    setTimeout(() => {
       expect(title).toBeInTheDocument();
       expect(closeButton).toBeInTheDocument();
       expect(description).toBeInTheDocument();
-    });
+      expect(title.textContent).toEqual('Money Tree Plant');
+      expect(description.textContent).toEqual(
+        'Said to bring good luck and fortune, the Money Tree is the perfect plant to add to any room of your home to create good Feng Shui. It is known for its resilience, ease of growth, and fun braided trunk.'
+      );
+    }, 2000);
+  });
+
+  it('closes the plant modal when the close button is clicked', async () => {
+    const setIsModalOpen = vi.fn();
+    render(<PlantModal id={testId} setIsModalOpen={setIsModalOpen} parent={document.body} />);
+    const closeButton = await findByTestId(document.body, 'close-button');
+    fireEvent.click(closeButton);
+    setTimeout(() => {
+      expect(closeButton).not.toBeInTheDocument();
+    }, 2000);
+  });
+
+  it('does not render if parent is null', () => {
+    const setIsModalOpen = vi.fn();
+    const { container } = render(
+      <PlantModal id={testId} setIsModalOpen={setIsModalOpen} parent={null} />
+    );
+    expect(container.firstChild).toBeNull();
+  });
+
+  it('renders the plant modal correctly if plant is not pet-friendly and not easy to care for', async () => {
+    const setIsModalOpen = vi.fn();
+    render(<PlantModal id="3" setIsModalOpen={setIsModalOpen} parent={document.body} />);
+    const title = await findByTestId(document.body, 'plant-title');
+    const description = await findByTestId(document.body, 'plant-description');
+    const petFriendlyIcon = document.querySelector('.modal-overlay-pet-friendly');
+    const easyCareIcon = document.querySelector('.modal-overlay-care');
+    setTimeout(() => {
+      expect(title).toBeInTheDocument();
+      expect(description).toBeInTheDocument();
+      expect(title.textContent).toEqual('Monstera');
+      expect(description.textContent).toEqual(
+        'The Monstera is a very popular indoor plant due to its unique foliage! It loves bright, indirect light, and can grow quite large if given the space. Its leaves have natural holes called fenestrations, which make it a favorite among plant enthusiasts.'
+      );
+      expect(petFriendlyIcon).not.toBeInTheDocument();
+      expect(easyCareIcon).not.toBeInTheDocument();
+    }, 2000);
   });
 });
